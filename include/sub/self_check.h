17122c609c479297c45009501fc3ac5ccca4b283
admin1 <1204018828@qq.com>
2023-02-18 12:12:31 +0800

    add duration

--- include/sub/self_check.h
+++ include/sub/self_check.h
@@ -81,7 +81,7 @@ public:
     bool isValid() {
         ros::Time now = ros::Time::now();
         last_valid_ = valid_;
-        valid_ = (now - last_image_ < ros::Duration(5) && now - last_pointcloud_ < ros::Duration(5));
+        valid_ = (now - last_image_ < ros::Duration(10) && now - last_pointcloud_ < ros::Duration(10));
         if(needPublish()){
             setPublish();
         }
@@ -145,7 +145,7 @@ public:
 
     bool isValid() {
         last_valid_ = valid_;
-        valid_ = ros::Time::now() - last_laser_ < ros::Duration(5);
+        valid_ = ros::Time::now() - last_laser_ < ros::Duration(10);
         if (needPublish()) {
           setPublish();
         }
@@ -194,7 +194,7 @@ public:
 
     bool isValid() {
         last_valid_ = valid_;
-        valid_ = ros::Time::now() - last_imu_ < ros::Duration(5);
+        valid_ = ros::Time::now() - last_imu_ < ros::Duration(10);
         if (needPublish()) {
           setPublish();
         }
