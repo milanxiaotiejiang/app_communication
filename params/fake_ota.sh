9aed5006604aaf9a1451d085934bccf5f578a5f5
admin1 <1204018828@qq.com>
2023-03-16 13:31:18 +0800

    change sensor topic name

--- params/fake_ota.sh
+++ params/fake_ota.sh
@@ -44,6 +44,9 @@ then
     cp /home/admin1/AirCore/cartographer_localization.lua /home/admin1/test_ws/install/share/robot_slam/params/
 fi
 echo "Updating 0.9.7"
+echo "Updating sensor topic names"
+rm /home/admin1/test_ws/install/share/robot_slam/params/sensor/realworld_sensor_params.yaml
+cp /home/admin1/AirCore/realworld_sensor_params.yaml /home/admin1/test_ws/install/share/robot_slam/params/sensor/
 echo "Updating inu"
 echo "123456" | sudo -S bash /home/admin1/AirCore/package18_2023_03_15/uninstall.sh
 sleep 5
