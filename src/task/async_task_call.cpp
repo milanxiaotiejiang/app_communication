989583d1bfdf418a33c99cc375d9815d4f5a6eee
songgang <18510824023@163.com>
2023-04-25 10:45:37 +0800

    add isPreparing

--- src/task/async_task_call.cpp
+++ src/task/async_task_call.cpp
@@ -948,6 +948,9 @@ void AsyncTaskCall::executeLift(bool lift) {
     if (isUnrecoverableError()) {
         return;
     }
+    if(!isPreparation(event_flow)){
+        return;
+    }
     if (lift) {
         notify_one([this]() {
             pushError(loop::error_epoll::error_lift);
