d8ee3734046b6f388a68da540645535660057ab3
admin1 <1204018828@qq.com>
2023-04-27 16:41:49 +0800

    fakeota.sh

--- params/fake_ota.sh
+++ params/fake_ota.sh
@@ -68,8 +68,8 @@ fi
 echo "Updating 0.9.9"
 echo "Upgrading libcartographer.so"
 echo "123456" | sudo -S cp /home/admin1/AirCore/libcartographer.so /usr/local/lib/
-echo "Upgrading cartographer_localization.lua"
-rm /home/admin1/test_ws/install/share/robot_slam/params/cartographer_localization.lua
-cp /home/admin1/AirCore/cartographer_localization.lua /home/admin1/test_ws/install/share/robot_slam/params/
+echo "Upgrading cartographer_rplidar.lua"
+rm /home/admin1/test_ws/install/share/robot_slam/params/cartographer_rplidar.lua
+cp /home/admin1/AirCore/cartographer_rplidar.lua /home/admin1/test_ws/install/share/robot_slam/params/
 
 
