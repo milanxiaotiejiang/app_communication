60fc89b884b3ca55883f70b9c74c7cb516041243
milan.xiaotiejiang <765151629@qq.com>
2023-05-04 17:24:21 +0800

    txt_upgrade

--- src/task/subscribe/ZooRobotStatusSubscribe.cpp
+++ src/task/subscribe/ZooRobotStatusSubscribe.cpp
@@ -22,7 +22,7 @@ ZooRobotStatusSubscribe::ZooRobotStatusSubscribe(ros::NodeHandle handle)
     sub_robot_status_ = handle.subscribe("/robot_status_inner", 1, &ZooRobotStatusSubscribe::subscribeCallback, this);
     sub_motor_error_ = handle.subscribe("/mrrobot/push_error", 10, &ZooRobotStatusSubscribe::motorErrorCallback, this);
     sub_hls_error_ = handle.subscribe("/mrrobot/hls_error", 10, &ZooRobotStatusSubscribe::hlsErrorCallback, this);
-    sub_wet_mop_error = handle.subscribe("/mrrobot/wet_mop_error", 10, &ZooRobotStatusSubscribe::hlsErrorCallback,
+    sub_wet_mop_error = handle.subscribe("/mrrobot/wet_mop_error", 10, &ZooRobotStatusSubscribe::wetMopErrorCallback,
                                          this);
     sub_laser_error_ = handle.subscribe("/lidar/restart", 10, &ZooRobotStatusSubscribe::laserErrorCallback, this);
 }
