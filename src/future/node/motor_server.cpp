ab34c85f75b808ef9d60cb2a880dd47f3b8e80bc
milan.xiaotiejiang <765151629@qq.com>
2023-05-12 15:20:15 +0800

    雷达启动

--- src/future/node/motor_server.cpp
+++ src/future/node/motor_server.cpp
@@ -7,22 +7,22 @@
 #include "glog/logging.h"
 
 void MotorServerSingleton::init(ros::NodeHandle handle) {
-    std::thread startServiceClientThread([this, &handle]() {
-        start_motor = handle.serviceClient<std_srvs::Empty>("start_motor");
-        start_motor.waitForExistence();
-        initialize_start = true;
-    });
-    startServiceClientThread.detach();
+    LOG(INFO) << "MotorServerSingleton  init ... ";
+    start_motor = handle.serviceClient<std_srvs::Empty>("start_motor");
+    start_motor.waitForExistence();
+    initialize_start = true;
 }
 
 bool MotorServerSingleton::start() {
     if (!Environment::instance().isRealEnvironment) {
         return true;
     }
-    LOG(INFO) << "ModeValidate  start_motor 开始启动雷达 ------------------------------ ";
+    LOG(INFO) << "MotorServerSingleton  start_motor 开始启动雷达 ... ";
     if (!initialize_start) {
         return false;
     }
     std_srvs::Empty start_cmd;
-    return start_motor.call(start_cmd);
+    bool callResult = start_motor.call(start_cmd);
+    LOG(INFO) << "MotorServerSingleton  callResult " << callResult << " ... ";
+    return callResult;
 }
