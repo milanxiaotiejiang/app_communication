12f28fe91f3e89cce797bd2ac63883dbcef41bd8
admin1 <1204018828@qq.com>
2023-06-02 09:42:07 +0800

    1204018828@qq.com

--- src/task/point_planner.cpp
+++ src/task/point_planner.cpp
@@ -78,19 +78,19 @@ void PointPlanner::goToPath(const RealBlock &block) {
     bool isLine = SqliteDataBase::TaskModeFromInt(block.mode) == TaskMode::Line;
     ros::NodeHandle nh;
     if (isLine) {
-        globalInflationRadius.d(0.3);
-        localInflationRadius.d(0.3);
         nh.setParam("/move_base/global_costmap/inflation_layer/inscribed_radius", 0.25);
         nh.setParam("/move_base/local_costmap/inflation_layer/inscribed_radius", 0.25);
         nh.setParam("/default_inscribed_radius", 0.25);
         nh.setParam("/default_inflation_radius", 0.3);
+        globalInflationRadius.d(0.3);
+        localInflationRadius.d(0.3);
     } else {
-        globalInflationRadius.d(0.5);
-        localInflationRadius.d(0.5);
         nh.setParam("/move_base/global_costmap/inflation_layer/inscribed_radius", 0.3);
         nh.setParam("/move_base/local_costmap/inflation_layer/inscribed_radius", 0.3);
         nh.setParam("/default_inscribed_radius", 0.3);
         nh.setParam("/default_inflation_radius", 0.5);
+        globalInflationRadius.d(0.5);
+        localInflationRadius.d(0.5);
     }
 
     replan_msgs::ReplanGoal path;
