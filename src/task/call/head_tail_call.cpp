93350c6fa1e8bd47cb58555dcc69d2460d49e55d
milan.xiaotiejiang <765151629@qq.com>
2023-06-02 09:47:28 +0800

    多次返回摆渡点失败， 直接记为“任务执行完成且返回了基站点”

--- src/task/call/head_tail_call.cpp
+++ src/task/call/head_tail_call.cpp
@@ -49,7 +49,10 @@ void HeadTailPointCall::handleFlowPoint(const RealPoint &point) {
                 rechargeRetryCount++;
                 setFlow(event::flow::try_recharging_again);
             } else {
-                setFlow(event::flow::software_interrupt_task);
+//                setFlow(event::flow::software_interrupt_task);
+                LOG(INFO) << "HeadTailPointCall : 多次返回摆渡点失败， 直接记为“任务执行完成且返回了基站点”， rechargeRetryCount : "
+                          << rechargeRetryCount << " ...";
+                setFlow(event::flow::flowing_water_execution_completed);
             }
         }
     } else if (point.id == FLOW_CLOSE_MECHANISM) {
