ad6af178f0669d030ea04a88039db47b6e770ce8
milan.xiaotiejiang <765151629@qq.com>
2023-06-02 11:11:10 +0800

    建图完成切为睡眠模式

--- src/task/call/head_tail_call.cpp
+++ src/task/call/head_tail_call.cpp
@@ -38,7 +38,11 @@ void HeadTailPointCall::handleFlowPoint(const RealPoint &point) {
                 backBaseRetryCount++;
                 setFlow(event::flow::try_move_base_point_again);
             } else {
-                setFlow(event::flow::software_interrupt_task);
+//                setFlow(event::flow::software_interrupt_task);
+                LOG(INFO) << "HeadTailPointCall : 多次返回摆渡点失败， 直接记为“任务执行完成且返回了基站点”， " <<
+                          "backBaseRetryCount : " << backBaseRetryCount <<
+                          "rechargeRetryCount : " << rechargeRetryCount << " ...";
+                setFlow(event::flow::flowing_water_execution_completed);
             }
         }
     } else if (point.id == FLOW_IN_STATION) {
@@ -49,10 +53,7 @@ void HeadTailPointCall::handleFlowPoint(const RealPoint &point) {
                 rechargeRetryCount++;
                 setFlow(event::flow::try_recharging_again);
             } else {
-//                setFlow(event::flow::software_interrupt_task);
-                LOG(INFO) << "HeadTailPointCall : 多次返回摆渡点失败， 直接记为“任务执行完成且返回了基站点”， rechargeRetryCount : "
-                          << rechargeRetryCount << " ...";
-                setFlow(event::flow::flowing_water_execution_completed);
+                setFlow(event::flow::software_interrupt_task);
             }
         }
     } else if (point.id == FLOW_CLOSE_MECHANISM) {
