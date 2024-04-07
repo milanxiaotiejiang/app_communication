5d367288efd037f284f98efcf0754715364c928e
milanxiaotiejiang <765151629@qq.com>
2024-04-07 09:57:25 +0800

    ros_version: 2.0.14.7_630

--- src/sub/json/MaterialStrategy.cpp
+++ src/sub/json/MaterialStrategy.cpp
@@ -76,6 +76,6 @@ bool MaintenanceModeStatusStrategy::handler(std::string params) {
 std::string SewagePumpSwitchStrategy::handler(bool params) {
     std_msgs::Int32 message;
     message.data = params ? 1 : 0;
-    PublishInnerManager::instance().pubMetalDetectionSwitch(message);
+    PublishInnerManager::instance().pubSewagePumpSwitch(message);
     return "";
 }
