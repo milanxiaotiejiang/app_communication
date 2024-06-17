0c9c0d4b1ee4edc9343804b7aab77617be9c300f
milanxiaotiejiang <765151629@qq.com>
2024-06-17 16:12:23 +0800

    /scan

--- src/future/node/hardware_subscriber.cpp
+++ src/future/node/hardware_subscriber.cpp
@@ -7,7 +7,7 @@
 void InuSubscriberSingleton::init(ros::NodeHandle handle) {
     subscriberInu1 = handle.subscribe("/1/depth/depth2pc", 1, &InuSubscriberSingleton::subscribeCallback1, this);
     subscriberInu2 = handle.subscribe("/2/depth/depth2pc", 1, &InuSubscriberSingleton::subscribeCallback2, this);
-    subscriberScan = handle.subscribe("/scan_raw", 1, &InuSubscriberSingleton::subscribeCallbackScan, this);
+    subscriberScan = handle.subscribe("/scan", 1, &InuSubscriberSingleton::subscribeCallbackScan, this);
 }
 
 void InuSubscriberSingleton::subscribeCallback1(const sensor_msgs::PointCloud2 &data) {
